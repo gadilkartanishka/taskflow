90b6030864c7256b07e6ad5c4a1af121f508baf2
Tanishka Gadilkar <gadilkartanishka@gmail.com>
2026-05-30 11:29:39 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,8 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT || 8000}`);
     });
   })
   .catch((err) => {
